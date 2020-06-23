042811e2184a54b42e50fb9cf695f5cd48af4bec
yamiroot <yamipqm@gmail.com>
2020-06-23 02:28:34 -0500

    Header para todas las vistas responsivas

---
+++ src/controller/header.js
@@ -0,0 +1,27 @@
+
+const iconMenu = () => {
+    const iconMenu = document.getElementById('icon-menu');
+    const headerHeight = document.getElementsByTagName('header')[0].offsetHeight;
+    const menuNav = document.getElementsByTagName('nav')[0];
+
+    const paddingHeight = headerHeight + 150;  // Alto de nav: 150px
+
+    let count = 0;
+
+    iconMenu.addEventListener('click', () => {
+        count++;
+
+        if (count % 2 !== 0) {
+            menuNav.classList.add('show');
+
+            document.body.style.paddingTop = paddingHeight.toString() + 'px';
+        }
+        else {
+            menuNav.classList.remove('show');
+
+            document.body.style.paddingTop = headerHeight.toString() + 'px';
+        }
+    });
+}
+
+iconMenu();
