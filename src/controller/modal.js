c5fd2011acc3fc326d2728f319759020719957a5
yamiroot <yamipqm@gmail.com>
2020-07-08 21:15:00 -0500

    Implementación de funcionalidad copiar respuesta cifrada o descifrada

--- src/controller/modal.js
+++ src/controller/modal.js
@@ -4,8 +4,6 @@ export const modalClose = (sectionModal) => {
   const buttonClose = sectionModal.querySelector('#button-close');
 
   buttonClose.addEventListener('click', () => {
-    console.log('gggggggggg', buttonClose);
-
     sectionModal.remove();
   });
 };
@@ -18,8 +16,37 @@ export const modalAccept = (sectionModal) => {
     document.querySelector('input').value = '';
     document.querySelector('textarea').value = '';
 
-    console.log('gggggggggg', buttonAccept);
-
     sectionModal.remove();
+  });
+};
+
+
+export const modalCopy = (sectionModal) => {
+  const buttonCopy = sectionModal.querySelector('#button-copy');
+  const responseCipher = sectionModal.querySelector('#response-cipher');
+  const toast = sectionModal.querySelector('span');
+
+  buttonCopy.addEventListener('click', () => {
+    const textArea = document.createElement('textarea');
+
+    textArea.value = responseCipher.innerHTML;
+
+    document.body.appendChild(textArea);
+
+    // Seleccionamos texto del textarea
+    textArea.select();
+
+    try {
+      // Copiando el texto seleccionado
+      const selectedText = document.execCommand('copy');
+
+      if (selectedText) toast.innerHTML = '¡Copiado!';
+
+      else toast.innerHTML = '¡Incapaz de copiar!';
+    } catch (err) {
+      toast.innerHTML = '¡Browser no soportado!';
+    }
+
+    document.body.removeChild(textArea);
   });
 };
--- src/view/view-modal.js
+++ src/view/view-modal.js
@@ -1,9 +1,9 @@
-import {modalClose, modalAccept} from '../controller/modal.js';
+import {modalClose, modalCopy, modalAccept} from '../controller/modal.js';
 
 
 const modal = (response, cipher) => {
   const contentModal = `
-    <section class="content-modal">
+    <section id="modal">
       <div class="modal-header">
         <h2>${(response.error) ? '¡Error!' : 'Respuesta'}</h2>
         <button id="button-close">&times;</button>
@@ -11,8 +11,9 @@ const modal = (response, cipher) => {
       <div class="modal-body">
         ${(response.error) ? `<p>${response.text}</p>` :
         `<p>El mensaje ${(cipher) ? 'cifrado' : 'descifrado'} es:</p>
-        <textarea disabled>${response.text}</textarea>
-        <button>Copiar</button>`}      
+         <div id="response-cipher">${response.text}</div>
+         <button id="button-copy">Copiar</button>
+         <span></span>`}      
       </div>
       <div class="modal-footer">
         <button id="button-accept">Aceptar</button>
@@ -21,10 +22,11 @@ const modal = (response, cipher) => {
   `;
 
   const sectionModal = document.createElement('div');
-  sectionModal.className = 'modal';
+  sectionModal.id = 'container-modal';
   sectionModal.innerHTML = contentModal;
 
   modalClose(sectionModal);
+  modalCopy(sectionModal);
   modalAccept(sectionModal);
 
   return sectionModal;
