bc6e36d5a9d34df4d7aaeae258eed65a78ac0b35
yamiroot <yamipqm@gmail.com>
2020-07-12 15:58:05 -0500

    Configuraión de htmlhint

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -9,7 +9,8 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       expect(typeof cipher.encode).toBe('function');
     });
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+    it(`debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para
+     "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33`, () => {
       // escribe aquí tus test
     });
     // Hacker edition
@@ -18,8 +19,10 @@ describe('cipher', () => {
     // Si decides agregar soporte para minúsculas descomenta el test a
     // continuación.
     //
-    // it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+    // it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para
+    // "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+    //   expect(cipher.encode(33,
+    // 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     // });
 
     // Hacker edition
@@ -37,7 +40,8 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       expect(typeof cipher.decode).toBe('function');
     });
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+    it(`debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para
+     "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33`, () => {
       // escribe aquí tus test
     });
     //
@@ -48,8 +52,10 @@ describe('cipher', () => {
     // continuación.
     //
     //
-    // it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+    // it('debería retornar "abcdefghijklmnopqrstuvwxyz"
+    // para "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+    //   expect(cipher.decode(33,
+    // 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
     // });
 
     // Hacker edition
